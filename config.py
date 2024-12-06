08dc1815a4c8f28301b6239c2ded2163deae9e8b
vvcb <vvcb.n1@gmail.com>
2024-12-06 22:55:16 +0000

    Add SQLMesh config

---
+++ config.py
@@ -0,0 +1,87 @@
+from dotenv import find_dotenv, load_dotenv
+
+import os
+from sqlmesh.core.config import Config
+
+from sqlmesh.core.config import (
+    ModelDefaultsConfig,
+    GatewayConfig,
+    MSSQLConnectionConfig,
+)
+from sqlmesh.core.config.format import FormatConfig
+from sqlmesh.core.config.ui import UIConfig
+from sqlmesh.core.model import ModelKindName
+from pydantic import BaseModel, ConfigDict
+from typing import Dict, Any
+
+load_dotenv(find_dotenv(), override=True)
+
+###############################################################################
+# SQLMESH CONFIGURATION
+###############################################################################
+
+state_schema: str = os.getenv("STATE_SCHEMA", "lth_omop_bronze")
+default_gateway: str = 'mssql'
+
+gateways = {}
+
+# Setup gateways
+
+# MSSQL
+
+gateway_mssql = GatewayConfig(
+    connection=MSSQLConnectionConfig(
+        host=os.environ["MSSQL_HOST"],
+        database=os.environ["MSSQL_DATABASE"],
+        concurrent_tasks=os.getenv("MSSQL_CONCURRENT_TASKS", default=4),
+    ),
+    state_connection=MSSQLConnectionConfig(
+        host=os.environ["MSSQL_STATE_DB_HOST"],
+        database=os.environ["MSSQL_STATE_DB_DATABASE"],
+    ),
+    state_schema=state_schema,
+)
+
+gateways["mssql"] = gateway_mssql
+
+
+class SQLMeshSettings(BaseModel):
+    """
+    SQLMeshSettings class represents the settings for SQLMesh.
+
+    Attributes:
+        project (str): The name of the project.
+        model_defaults (ModelDefaultsConfig): The configuration for model defaults.
+        gateways (dict): A dictionary of gateways, where the keys are the gateway names and the values are the corresponding gateway functions.
+        default_gateway (str): The name of the default gateway.
+        variables (dict): A dictionary of variables.
+        format (FormatConfig): The configuration for formatting.
+    """
+
+    model_config = ConfigDict(protected_namespaces=())
+
+    project: str
+    model_defaults: ModelDefaultsConfig = ModelDefaultsConfig(
+        kind=ModelKindName.VIEW, dialect="mssql", cron="@daily", owner="LTH DST",
+        start='2024-01-01'
+    )
+    gateways: Dict[str, GatewayConfig] = gateways
+    default_gateway: str = default_gateway
+    variables: Dict[str, Any]
+    format: FormatConfig = FormatConfig(
+        append_newline=True,
+        normalize=True,
+        indent=2,
+        normalize_functions="lower",
+        leading_comma=False,
+        max_text_width=80,
+        no_rewrite_casts=False,
+    )
+    ui: UIConfig = UIConfig(format_on_save=True)
+
+variables = {
+        "global_start_date": "2005-01-01",
+        "minimum_observation_period_start_date": "2005-01-01",
+    }
+
+config = Config(**dict(SQLMeshSettings(project="stroma", variables=variables)))
