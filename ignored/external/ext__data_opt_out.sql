d12f2e43dfa40b56a599255ea47b6609be6415a6
vvcb <vvcb.n1@gmail.com>
2024-12-07 19:16:38 +0000

    Move data opt-out logic to ignored

--- ignored/external/ext__data_opt_out.sql
+++ ignored/external/ext__data_opt_out.sql
@@ -5,12 +5,14 @@ MODEL (
   cron '@daily',
 );
 
+-- The workflow around NDOO needs rethinking to remove manual processes.
+
 select
   MRN as mrn,
   NHSNumber as nhs_number,
   mpi.person_id,
   cast(mpi.person_source_value as bigint) as person_source_value,
   InsertDateTime as insert_datetime
-from admin.Data_Opt_Out doo
+from @catalog_src.admin.Data_Opt_Out doo
 left join lth_bronze.stg__master_patient_index mpi
 on doo.MRN = mpi.flex_mrn
