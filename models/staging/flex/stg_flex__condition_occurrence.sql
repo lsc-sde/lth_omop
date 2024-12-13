ce3bcca6dc6d23dfcab6cecdeb55cf5fa9f27464
vvcb <vvcb.n1@gmail.com>
2024-12-13 20:57:16 +0000

    Convert cond occ partially to incremental

--- models/staging/flex/stg_flex__condition_occurrence.sql
+++ models/staging/flex/stg_flex__condition_occurrence.sql
@@ -1,46 +1,37 @@
 
 MODEL (
   name lth_bronze.stg_flex__condition_occurrence,
-  kind FULL,
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column (last_edit_time, '%Y-%m-%d %H:%M:%S.%f'),
+    batch_size 30
+  ),
   cron '@daily',
+  grain (patient_id, visit_occurrence_id, source_code, data_source)
 );
 
-with condition_occ as (
-  select
-    visit_number,
-    episode_start_dt,
-    episode_end_dt,
-    index_nbr,
-    provider_source_value,
-    last_edit_time,
-    updated_at,
-    replace(
-      left(icd10_code, 3) + '.' + substring(icd10_code, 4, 10), '.X', ''
-    ) as source_code
-  from lth_bronze.src_flex__vtg_diagnosis 
-),
-
-visit_detail as (
+with visit_detail as (
   select distinct
     visit_number,
     visit_id,
     first_visit_id,
     person_source_value
-  from lth_bronze.stg_flex__facility_transfer 
+  from lth_bronze.stg_flex__facility_transfer
 )
 
 select
   isnull(v.person_source_value, vs.person_source_value) as patient_id,
-  c.episode_start_dt,
-  c.episode_end_dt,
-  c.provider_source_value as provider_id,
-  c.source_code,
-  c.index_nbr,
-  c.last_edit_time,
-  c.updated_at,
+  sfvd.episode_start_dt,
+  sfvd.episode_end_dt,
+  sfvd.provider_source_value as provider_id,
+  replace(
+      left(sfvd.icd10_code, 3) + '.' + substring(sfvd.icd10_code, 4, 10), '.X', ''
+    ) as source_code,
+  sfvd.index_nbr,
+  sfvd.last_edit_time,
+  sfvd.updated_at,
   isnull(v.first_visit_id, vs.visit_id) as visit_occurrence_id,
   'ukcoder' as data_source
-from condition_occ as c
+from lth_bronze.src_flex__vtg_diagnosis  as sfvd
 left join
   (
     select distinct
@@ -50,20 +41,21 @@ left join
     from
       visit_detail
   ) as v
-  on c.visit_number = v.visit_number
+  on sfvd.visit_number = v.visit_number
 left join
   (
     select distinct
       visit_number,
       visit_id,
       person_source_value
-    from lth_bronze.src_flex__visit_segment 
+    from lth_bronze.src_flex__visit_segment
     where
       visit_number not in (select distinct visit_number from visit_detail)
   ) as vs
   on
-    c.visit_number = vs.visit_number
+    sfvd.visit_number = vs.visit_number
     and v.first_visit_id is null
+    and sfvd.last_edit_time between @start_ds and @end_ds
 
 union
 
@@ -93,3 +85,4 @@ left join
       visit_detail
   ) as v
   on ae.visit_id = v.visit_id
+where ae.last_edit_time between @start_ds and @end_ds
