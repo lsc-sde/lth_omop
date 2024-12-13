0afcca847754be2db3bd7a761353dc6f6ee1de6c
vvcb <vvcb.n1@gmail.com>
2024-12-13 20:57:16 +0000

    Convert cond occ partially to incremental

--- models/staging/flex/stg_flex__ae_diagnosis.sql
+++ models/staging/flex/stg_flex__ae_diagnosis.sql
@@ -1,11 +1,15 @@
 
 MODEL (
   name lth_bronze.stg_flex__ae_diagnosis,
-  kind FULL,
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column (last_edit_time, '%Y-%m-%d %H:%M:%S.%f'),
+    batch_size 30
+  ),
   cron '@daily',
   audits (
     not_null(columns := (visit_id, patient_id, last_edit_time, updated_at, diagnosis_list))
-  )
+  ),
+  grain (patient_id, visit_id, diagnosis_list)
 );
 
 select
@@ -17,7 +21,7 @@ select
   discharge_date_time,
   last_edit_time,
   updated_at,
-  value as diagnosis_list
+  s2.value as diagnosis_list
 from (
   select
     visit_id,
@@ -27,11 +31,13 @@ from (
     discharge_date_time,
     last_edit_time,
     updated_at,
-    value as diag_list
-  from lth_bronze.src_flex__ae_diagnosis 
-  cross apply string_split(diag_list, '~')
+    s1.value as diag_list
+  from lth_bronze.src_flex__ae_diagnosis
+  where last_edit_time between @start_ds and @end_ds
+  cross apply string_split(diag_list, '~') as s1
 ) as t
-cross apply string_split(diag_list, '|')
+cross apply string_split(diag_list, '|') as s2
 where
   len(value) > 1
   and value not in ('410605003')
+  and last_edit_time between @start_ds and @end_ds
