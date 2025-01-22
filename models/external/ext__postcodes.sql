6eb1169c2b234cba5151c76d15ada158ae7f1a3d
vvcb <vvcb.n1@gmail.com>
2025-01-22 13:44:21 +0000

    Complete Location table lineage AB#1353
    - This builds directly from `ext__postcodes` and removes stg__location and vocab__location (AB#1764)
    - New Python model that generates unique integers for valid postcodes (AB#1765)
    - Invalid postcodes are assigned `location_id=0`
    
    Tested with the following query.
    
    ```sql
    select 'zip' as c, count(distinct zip) as n from lth_bronze__vc.location l
    union select 'location_id' as c, count(distinct location_id) as n from lth_bronze__vc.location l
    union select 'total_rows' as c, count(*) as n from lth_bronze__vc.location l
    union select 'invalid_postcodes' as c, count(*) as n from lth_bronze__vc.location l where l.location_id = 0
    ````
    
    Returns:
    
    |c|n|
    |-|-|
    |zip|2683735|
    |location_id|2683735|
    |total_rows|2683735|
    |invalid_postcodes|0|

--- models/external/ext__postcodes.sql
+++ models/external/ext__postcodes.sql
@@ -7,6 +7,5 @@ MODEL (
 {# ToDo: The source for postcodes data is hardcoded and needs to be ref. #}
 select
   p.pcd7 as postcode,
-  p.lsoa21cd as location_source_value,
-  cast(dense_rank() over (order by p.lsoa21cd) as bigint) as location_id
+  p.lsoa21cd as location_source_value
 from dbt_omop.admin.postcodes as p
