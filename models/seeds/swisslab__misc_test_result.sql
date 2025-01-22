61849df5bca5b0904898c67c80367dddae453090
vvcb <vvcb.n1@gmail.com>
2025-01-22 20:49:41 +0000

    Complete documentation of seed models AB#1723

--- models/seeds/swisslab__misc_test_result.sql
+++ models/seeds/swisslab__misc_test_result.sql
@@ -3,5 +3,7 @@ MODEL (
   name lth_bronze.swisslab__misc_test_result,
   kind SEED (
     path '$root/seeds/swisslab__misc_test_result.csv'
-  )
+  ),
+    description 'Vocabulary mappings for miscellaneous test results from SwissLab at LTH.'
+
 );
