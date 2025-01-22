75ec73a4a043a6e53c423282feefa26e67c6613f
vvcb <vvcb.n1@gmail.com>
2025-01-22 20:51:28 +0000

    Improve column datatype casting

--- models/omop/source_to_concept_map.sql
+++ models/omop/source_to_concept_map.sql
@@ -6,16 +6,16 @@ MODEL (
 );
 
 select
-  cast(source_code as varchar(50)) as source_code,
-  cast(0 as bigint) as source_concept_id,
-  cast(44819096 as varchar(20)) as source_vocabulary_id,
-  cast(source_code_description as varchar(255)) as source_code_description,
-  cast(target_concept_id as bigint) as target_concept_id,
-  cast(c.vocabulary_id as varchar(20)) as target_vocabulary_id,
-  c.valid_start_date,
-  c.valid_end_date,
-  c.invalid_reason,
-  frequency
+  source_code::VARCHAR(50) as source_code,
+  0::INT as source_concept_id,
+  44819096::VARCHAR(8) as source_vocabulary_id,
+  source_code_description::VARCHAR(255) as source_code_description,
+  target_concept_id::BIGINT as target_concept_id,
+  c.vocabulary_id::VARCHAR(20) as target_vocabulary_id,
+  c.valid_start_date::DATE,
+  c.valid_end_date::DATE,
+  c.invalid_reason::VARCHAR,
+  frequency::INT
 from lth_bronze.vocab__source_to_concept_map_raw as r
 left join @catalog_src.@schema_vocab.CONCEPT as c
   on r.target_concept_id = c.concept_id
