71b2779ad328fa681443dea35ac67009dbe33c0e
vvcb <vvcb.n1@gmail.com>
2025-01-22 20:53:10 +0000

    Include DQ and deduplication in LOCATION AB#1353

--- models/omop/location.py
+++ models/omop/location.py
@@ -5,34 +5,51 @@ from string import ascii_lowercase, digits
 import pandas as pd
 from sqlmesh import ExecutionContext, model
 from sqlmesh.core.model.kind import ModelKindName
+from sqlglot.expressions import to_column
 
 alphanum = ascii_lowercase + digits
 charmap = {a: str(n) for a, n in zip(alphanum, range(10, 10 + 36))}
 
 
-def postcode_to_unique(postcode: str) -> int:
+def normalise_postcode(postcode: str) -> str:
     """
-    Converts a given postcode to a unique integer representation.
-    This function takes a postcode string, normalizes it by converting it to lowercase
-    and removing any spaces, and then maps each character to a corresponding integer
-    using a predefined character map. The resulting integers are concatenated to form
-    a unique integer representation of the postcode.
+    Normalises a given postcode by converting it to lowercase and removing any spaces.
+    If the postcode contains any non-alphanumeric characters, it is considered invalid
+    and the function returns "badcode".
     Args:
-        postcode (str): The postcode to be converted.
+        postcode (str): The postcode to be normalised.
     Returns:
-        int: The unique integer representation of the postcode. Returns 0 if the postcode
-        contains invalid characters.
+        str: The normalised postcode or "badcode" if the postcode is invalid.
     """
 
     try:
         postcode = postcode.lower().replace(" ", "")
         assert all(p in alphanum for p in postcode)
 
+    except AssertionError as e:
+        print(e)
+        postcode = "badcode"
+
+    finally:
+        return postcode
+
+
+def postcode_to_unique(postcode: str) -> int:
+    """
+    Converts a postcode string to a unique integer representation.
+    Args:
+        postcode (str): The postcode to be converted.
+    Returns:
+        int: The unique integer representation of the postcode. If an error occurs, returns 0.
+    Raises:
+        AssertionError: If the postcode contains characters not in the charmap.
+    """
+
+    try:
         out = "".join(str(charmap[p]) for p in postcode)
     except AssertionError as e:
         print(e)
         out = 0
-
     finally:
         return int(out)
 
@@ -58,7 +75,11 @@ columns = {
     depends_on=["lth_bronze.ext__postcodes"],
     kind=ModelKindName.FULL,
     columns=columns,
-    cron="@daily",
+    cron="@hourly",
+    audits=[
+        ("not_null", {"columns": [to_column("location_id"), to_column("zip")]}),
+        ("unique_values", {"columns": [to_column("location_id"), to_column("zip")]}),
+    ],
 )
 def execute(
     context: ExecutionContext,
@@ -71,12 +92,15 @@ def execute(
 
     df = context.fetchdf(query=f"select postcode, location_source_value from {table}")
 
-    df["location_id"] = df["postcode"].apply(postcode_to_unique)
+    df["zip"] = df["postcode"].apply(normalise_postcode)
+
+    # Remove duplicate postcodes
+    df = df.drop_duplicates(subset=["zip"])
+    df["location_id"] = df["zip"].apply(postcode_to_unique)
     df["address_1"] = None
     df["address_2"] = None
     df["city"] = None
     df["state"] = None
-    df["zip"] = df["postcode"]
     df["county"] = None
     df["country_concept_id"] = 42035286
     df["country_source_value"] = "United Kingdom of Great Britain and Northern Ireland"
