0ac7520975f28682e519a38a3e758b912b3c59b3
quindavies <92085506+quindavies@users.noreply.github.com>
2025-01-28 10:18:34 +0000

    CARE_SITE lineage (complete)

---
+++ models/omop/care_site.sql
@@ -0,0 +1,18 @@
+
+MODEL (
+  name lth_bronze.care_site,
+  kind FULL,
+  cron '@daily',
+);
+
+select distinct
+  care_site_id::bigint as care_site_id,
+  care_site_name::varchar(255) as care_site_name,
+  place_of_service_concept_id::bigint as place_of_service_concept_id,
+  location_id::bigint as location_id,
+  care_site_source_value::varchar(50) as care_site_source_value,
+  place_of_service_source_value::varchar(50)
+    as place_of_service_source_value
+from lth_bronze.vocab__care_site as cs
+left join lth_bronze.location as l
+  on cs.location_source_value = l.location_source_value
