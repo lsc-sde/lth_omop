7ce8f1dce04aa1d31bbeff848a287e43b5941d89
quindavies <92085506+quindavies@users.noreply.github.com>
2025-01-28 10:19:00 +0000

    PROVIDER lineage (complete)

---
+++ models/vocab/vocab__provider.sql
@@ -0,0 +1,27 @@
+
+MODEL (
+  name lth_bronze.vocab__provider,
+  kind FULL,
+  cron '@daily',
+);
+
+select
+  provider_name,
+  care_site_id,
+  provider_source_value,
+  provider_id,
+  specialty_source_value,
+  vm.source_code_description,
+  vm.target_concept_id,
+  cons_org_code
+from lth_bronze.stg__provider as p
+left join
+  (
+    select distinct
+      source_code_description,
+      target_concept_id
+    from lth_bronze.vocab__source_to_concept_map
+    where [group] = 'specialty'
+  ) as vm
+  on p.specialty_source_value = vm.source_code_description
+  
