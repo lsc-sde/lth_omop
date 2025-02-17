01444d03baaff389bad7936e7bba77a08bd40844
quindavies <92085506+quindavies@users.noreply.github.com>
2025-02-17 14:39:21 +0000

    SPECIMEN workflow

---
+++ models/omop/specimen.sql
@@ -0,0 +1,29 @@
+
+MODEL (
+  name lth_bronze.specimen,
+  kind FULL,
+  cron '@daily',
+  kind INCREMENTAL_BY_UNIQUE_KEY (
+    unique_key unique_key
+  )
+);
+
+select
+  row_number() over (order by NEWID())::bigint as specimen_id,
+  person_id::bigint as person_id,
+  specimen_concept_id::bigint as specimen_concept_id,
+  specimen_type_concept_id::bigint as specimen_type_concept_id,
+  specimen_date::date as specimen_date,
+  specimen_date::datetime as specimen_datetime,
+  NULL::float as quantity,
+  NULL::bigint as unit_concept_id,
+  anatomic_site_concept_id::bigint as anatomic_site_concept_id,
+  NULL::bigint as disease_status_concept_id,
+  specimen_source_id::varchar(50) as specimen_source_id,
+  specimen_source_value::varchar(50) as specimen_source_value,
+  NULL::varchar(50) as unit_source_value,
+  anatomic_site_source_value::varchar(50) as anatomic_site_source_value,
+  NULL::varchar(50) as disease_status_source_value,
+  measurement_event_id as specimen_event_id,
+  updated_at::datetime as updated_at
+from lth_bronze.vocab__specimen
---
+++ models/staging/stg__specimen.sql
@@ -0,0 +1,37 @@
+
+MODEL (
+  name lth_bronze.stg__specimen,
+  kind FULL,
+  cron '@daily',
+);
+
+with person as (
+	select  
+		mpi.nhs_number,  
+		mpi.person_id,  
+		mpi.person_source_value 
+	from lth_bronze.stg__master_patient_index mpi  
+	where mpi.current_record = 1  
+	and exists (
+		select 1 from lth_bronze.stg_sl__bacteriology sl where sl.nhs_number = mpi.nhs_number
+  )
+)
+
+select distinct
+  mpi.person_id,
+  mpi.person_source_value,
+  sl_ba.nhs_number::varchar(20),
+  sl_ba.order_date,
+  sl_ba.site::varchar(100),
+  qualifier::varchar(20),
+  order_number::varchar(20),
+  measurement_event_id,
+  org_code::varchar(5),
+  source_system::varchar(20),
+  updated_at
+from lth_bronze.stg_sl__bacteriology as sl_ba
+inner join person as mpi
+  on sl_ba.nhs_number = mpi.nhs_number
+where
+  sl_ba.site is not null
+  and sl_ba.order_date is not null
